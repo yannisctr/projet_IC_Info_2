315798374eca009b30396d3ef72a8079e6b49052
Lina GR <130139096+LinaGR07@users.noreply.github.com>
2024-06-16 21:20:40 +0200

    Update traitementconge.php

--- Salariés/traitementconge.php
+++ Salariés/traitementconge.php
@@ -16,6 +16,7 @@
 
         <nav>
             <a href="Infopage.php" class="link">Accueil</a>
+            <a href="mesInfos.php" class="link">Mes infos</a>
             <a href="deconnexion.html" class="link">Déconnexion</a>  
         </nav>
     
