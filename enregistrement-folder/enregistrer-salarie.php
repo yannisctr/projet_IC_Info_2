11681fe06db45397b5bb9a7931b4e38b5ab05b4d
Lina GR <130139096+LinaGR07@users.noreply.github.com>
2024-08-11 12:02:53 +0200

    Update enregistrer-salarie.php

--- enregistrement-folder/enregistrer-salarie.php
+++ enregistrement-folder/enregistrer-salarie.php
@@ -18,7 +18,6 @@ if (!isset($_SESSION['profil'])) {
 </head>
 <body>
 <div class="header">
-<div class="header">
         <img class="logo" src="../img-package/logo_alb.png" alt="logo-alb">
         <button class="b1" type="button" onclick="location.href = '../Administrateur/accueil-admin.php';">Accueil</button>
     </div>
