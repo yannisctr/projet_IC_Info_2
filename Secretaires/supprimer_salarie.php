e6a2a6e88904a5d050a7513a0c285d9198a0d7fc
Lina GR <130139096+LinaGR07@users.noreply.github.com>
2024-08-31 15:08:17 +0200

    mise à jour des css pour le responsive

--- Secretaires/supprimer_salarie.php
+++ Secretaires/supprimer_salarie.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['profil'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css-folder/ajouter-salarie-style.css">
     <title>Enregistrement de la suppression</title>
 </head>
 <body>
